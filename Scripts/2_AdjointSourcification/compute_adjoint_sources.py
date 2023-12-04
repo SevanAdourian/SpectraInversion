Fixed adjoint source computation

Scripts/2_AdjointSourcification/compute_adjoint_sources.py
@@ -11,63 +11,117 @@ import pyasdf
 
 from scipy.signal import periodogram, coherence
 from scipy.optimize import fmin
+from scipy.interpolate import interp1d
 
 import ProcessSeismicData as PSD
 
-def load_data(asdf_file, tr_id):
-    with pyasdf.ASDFDataSet(filename_asdf_pro, compression="gzip-3", mode='r+') as ds:
-        spec_ds = ds.auxiliary_data.ProcessedSpectra[tr_id]
-        sfreq = spec_ds.parameters["start_freq"]
-        nfreq = spec_ds.parameters["nfreq"]
-        dfreq = spec_ds.parameters["dfreq"]
-        efreq = sfreq+(nfreq-1)*dfreq
-        freq_array = np.arange(sfreq, efreq+dfreq, dfreq)
+class SynthSpectrum:
+    def __init__(self, frequency, real_part, imaginary_part, absolute_value):
+        self.frequency = frequency
+        self.real = real_part
+        self.imag = imaginary_part
+        self.abs_val = absolute_value
 
-        return freq_array, spec_ds
+class SynthSpectrumCollection:
+    def __init__(self):
+        self.records = {}
+
+    def add_record(self, station, record):
+        if station not in self.records:
+            self.records[station] = []
+        self.records[station].append(record)
+
+def load_data(spec_ds):
+    sfreq = spec_ds.parameters["start_freq"]
+    nfreq = spec_ds.parameters["nfreq"]
+    dfreq = spec_ds.parameters["dfreq"]
+    efreq = sfreq+(nfreq-1)*dfreq
+    freq_array = np.arange(sfreq, efreq+dfreq, dfreq)
+    data_spec = np.array(spec_ds.data)
+        
+    return freq_array, data_spec
 
 def load_synthetic(conf, event, stn):
+    
+    file_path = f"{conf['basedir']}Scripts/1_SyntheticComputation/results/{event}/{conf['starting_model']}/{stn}.{comp}.dat"
+    try:
+        frequency, real, imag, abs_val = [], [], [], []
+        
+        with open(file_path, 'r') as file:
+            for line in file:
+                if line.strip():  # Skip empty lines
+                    columns = line.split()
+                    frequency.append(float(columns[0]))
+                    real.append(float(columns[1]))
+                    imag.append(float(columns[2]))
+                    abs_val.append(float(columns[3]))
 
-    return freq_array, synth_spec
+                    synth_record = SynthSpectrum(np.array(frequency), np.array(real),
+                                                 np.array(imag), np.array(abs_val))
 
-def is_synthetic_computed(conf, event, stn):
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+    return synth_record
 
+def is_synthetic_computed(conf, event, stn, comp):
+
+    path_to_look = f"{conf['basedir']}Scripts/1_SyntheticComputation/results/{event}/{conf['starting_model']}/{stn}.{comp}.dat"
+    print(path_to_look)
+    if os.path.exists(path_to_look):
+        synth_ok = True
+        print(f"Found synthetic for event {event} and station {stn}")
+        print(f"Continuing the workflow")
+    else:
+        synth_ok = False
+        print(f"The synthetic for event {event} and station {stn} does not exist")
+        print(f"Skipping...")
     return synth_ok
 
-def process_synthetic(conf, freq_support, spec, dt, f1, f2, f12, f21, t1, t2, t12, t21, ep):
-    # From the IDSM computation, process the synthetic seismogram in the
-    # same way as data
+def process_synthetic(conf, synth_spec, dt, t1, t2, t12, t21, ep):
 
-    nt = len(spec)
-    # Compute inverse fourier transform to get seismogram in velocity
-    seismogram = np.real(np.fft.ifft(spec))
-    time_support = np.arange(0, nt) * dt
+    # Do ifft to get seismogram in time domain (real part)
+    ns = len(synth_spec.real)
+    synth_spec_complex = np.zeros(ns, dtype=complex)
+    synth_spec_complex.real = synth_spec.real
+    synth_spec_complex.imag = synth_spec.imag
+    synth_seismo = np.fft.irfft(synth_spec_complex)
+    synth_seismo = np.real(synth_seismo)
 
-    windowed_seismogram = np.zeros(nt,dtype=complex)
-
-    for i,t_value in np.ndenumerate(time_support):
-        hann_coeff = hann(t_value, t1, t12, t21, t2)
-        windowed_seismogram[i] = hann_coeff*seismogram[i]*np.exp(ep*t_value)
-
-    processed_spec = np.fft.fft(windowed_seismogram)
+    # Compute time support array
+    time_support = np.arange(0, dt*len(synth_spec.real)-1e-6, dt)
     
-    return processed_spec
+    # Correct epsilon and time domain
+    synth_seismo_windowed = np.zeros(ns)
+    for i,t_value in np.ndenumerate(time_support):
+        hann_coeff = PSD.hann(t_value, t1, t12, t21, t2)
+        synth_seismo_windowed[i] = hann_coeff*synth_seismo[i]*np.exp(-ep*t_value)
 
-def compute_misfit(synth_freq_array, processed_synth_spec, data_freq_array, data_spec_ds):
-    # We interpolate the spectra on the same frequency support as the synthetic
-    # The adjoint source should be sampled the same way in order to multiply it directly
-    # for the adjoint computation
+    # Back to spectral domain (fft)
+    processed_synth_spec = np.fft.fft(synth_seismo_windowed)
+    f = np.fft.fftfreq(ns, d=dt)
+    # Window in frequency domain
+    processed_synth_spec = processed_synth_spec[(f >= param['low_corner']/1e3) & (f <= (param['high_corner']+0.2)/1e3)]
+    freq_support_pro = f[(f >= param['low_corner']/1e3) & (f <= (param['high_corner']+0.2)/1e3)]
 
-    data_spec = np.array(data_spec_ds.data)
-    interpolator = interp1d(data_freq_array, data_spec, kind='linear', fill_value='extrapolate')
-    interpolated_data_spec = interpolator(synth_freq_array)
+    # processed_synth_spec = synth_seismo
+    
+    # return time_support, processed_synth_spec
+    return freq_support_pro,processed_synth_spec
 
-    adj_src = np.zeros(
-    adj_src = processed_synth_spec - interpolated_data_spec
-    return adj_src
+# def compute_misfit(processed_synth_spec, data_spec_ds):
 
-def process_adjoint_sources(conf, l2_misfit):
+#     interpolator = interp1d(data_freq_array, data_spec, kind='linear', fill_value='extrapolate')
+#     interpolated_data_spec = interpolator(processed_synth_spec)
 
-    return pro_adj_src
+#     adj_src = np.zeros(len(processed_synth_spec))
+#     adj_src = processed_synth_spec.real - data_spec_ds.real 
+#     return adj_src
+
+# def process_adjoint_sources(conf, adj_src):
+    
+#     return pro_adj_src
 
 
 if __name__ == "__main__":
@@ -82,7 +136,7 @@ if __name__ == "__main__":
     param = PSD.load_yaml_file(yaml_file_name)    
 
     # Generate sample data points
-    f1_inp = param['minimum_frequency']/1.e3
+    f1_inp = param['minimum_frequency']
     f2_inp = param['maximum_frequency']/1.e3
     t1_inp = param['start_time_series'] * 3600.
     t2_inp = param['end_time_series'] * 3600.
@@ -94,98 +148,131 @@ if __name__ == "__main__":
     f1 = param['low_corner']/1.e3  # low corner
     f2 = param['high_corner']/1.e3 # high corner
 
+    # Useful for rescaling synthetic - due to technical considerations of IDSM
+    mex = 5
+    ep = mex/t2_inp
+        
     # Computation of all parameters
     f12 = f1+facf*(f2-f1)
     f21 = f2-facf*(f2-f1)
-    t12 = t1+fact*(t2-t1)
-    t21 = t2-fact*(t2-t1)
+    t12 = t1_inp+fact*(t2_inp-t1_inp)
+    t21 = t2_inp-fact*(t2_inp-t1_inp)
         
-    ##################    
     # Load asdf file
     events    = glob.glob(f"{param['basedir']}Data/*")
 
-    # Initialize the inventory for event
-    inv = []
-    
     for _i, event_path in enumerate(events):
         # Print some stuff here
         event = os.path.basename(event_path)
         print(event)
-        print(f"{event_path}/asdf/{event}.h5")
 
         # load asdf
-        filename_asdf = f"{event_path}/asdf/{event}.h5"
-        with pyasdf.ASDFDataSet(filename_asdf_pro, compression="gzip-3", mode='r+') as ds:
-            for stations in ds.waveforms:
-                # Reconstruct inventory
-                inv += stations.StationXML
+        filename_asdf = f"{event_path}/asdf/{event}_pro.h5"
+        with pyasdf.ASDFDataSet(filename_asdf, compression="gzip-3", mode='r+') as ds:
+            for spec_ds in ds.auxiliary_data.ProcessedSpectra:
+                # Get station id
+                stn_full = spec_ds.path
+                ntw  = stn_full.split('.')[0]
+                stn  = stn_full.split('.')[1]
+                comp = stn_full.split('.')[3][-1]
 
-                st = stations.raw_waveform
-                # Select the waveform data
-                for i, tr in enumerate(st):
-                    NFFT = 2 ** (math.ceil(math.log(tr.stats.npts, 2)))
-
-                    # demean/detrend
-                    tr = tr.detrend('demean')
-                    tr = tr.detrend('linear')
+                if stn=='KIEV':
+                    # Check if we have computed synthetic for this station/event pair
+                    # for now only deals with Z
+                    is_synth = is_synthetic_computed(param, event, stn, comp)
                     
-                    # Remove instrument respones
-                    tr = tr.remove_response(inventory=inv, output='ACC', water_level=60, plot=False,
-                                            pre_filt=[0.0001, 0.00011, 0.0051, 0.0052])
+                    if is_synth:
+                        # We have computed synthetic, let's compute adjoint sources
+                        print(f"Synthetics were computed for this event/station pair")
+                        print(f"Loading synthetic spectra...")
+                        synth_spectrum = load_synthetic(param, event, stn)
+                        
+                        # Process synthetic spectra the same way as data to be consistent
+                        freq_pro, synth_spectrum_pro = process_synthetic(param, synth_spectrum, dt,
+                                                                           t1_inp, t2_inp, t21, t12, ep)
+                        # plt.plot(time_array/3600., synth_spectrum_pro)
+                        
+                        # Now let's load data
+                        freq_data, data_spec = load_data(spec_ds)
+                        plt.figure()
+                        plt.plot(freq_pro*1e3, np.abs(synth_spectrum_pro)/1000/np.pi)
+                        plt.plot(freq_data,data_spec/1e9)
+                        plt.show()
+                        
+                        # Compute L2-misfit between synth and data
+                        # adj_src = compute_misfit(synth_spectrum_pro, freq_array, data_spec)
+                        
+                    else:
+                        # No synthetic was computed, either data is not good, or the specific path not needed
+                        print(f"No synthetic was found, skipping station {stn}")
 
-                    # Removing detiding because tides out of band
-                    # detiding_ds = int(1/(0.0001*2*tr.stats.delta))
-                    # tr = PSD.detide(tr, param['order_detiding'], detiding_ds, plot=False, plot_obspy=False)
+                else:
+                    pass
+                # # Select the spectra data
+                # for i, tr in enumerate(st):
+                #     NFFT = 2 ** (math.ceil(math.log(tr.stats.npts, 2)))
 
-                    # Doing a second pass for windowing
-                    tr = PSD.trim_tr(tr, param['start_time_series'], param['end_time_series'], plot=False) # Trim
-                    tr = tr.taper(type=param['taper_type'], max_percentage=fact)
-                    tr = tr.detrend('linear')
+                #     # demean/detrend
+                #     tr = tr.detrend('demean')
+                #     tr = tr.detrend('linear')
+                    
+                #     # Remove instrument respones
+                #     tr = tr.remove_response(inventory=inv, output='ACC', water_level=60, plot=False,
+                #                             pre_filt=[0.0001, 0.00011, 0.0051, 0.0052])
+
+                #     # Removing detiding because tides out of band
+                #     # detiding_ds = int(1/(0.0001*2*tr.stats.delta))
+                #     # tr = PSD.detide(tr, param['order_detiding'], detiding_ds, plot=False, plot_obspy=False)
+
+                #     # Doing a second pass for windowing
+                #     tr = PSD.trim_tr(tr, param['start_time_series'], param['end_time_series'], plot=False) # Trim
+                #     tr = tr.taper(type=param['taper_type'], max_percentage=fact)
+                #     tr = tr.detrend('linear')
                                         
-                    # Compute fft
-                    f, acc = periodogram(tr.data, fs=tr.stats.sampling_rate, nfft=NFFT,
-                                       scaling='spectrum')
-                    f, acc = f[1:], acc[1:]
+                #     # Compute fft
+                #     f, acc = periodogram(tr.data, fs=tr.stats.sampling_rate, nfft=NFFT,
+                #                        scaling='spectrum')
+                #     f, acc = f[1:], acc[1:]
 
-                    # Hann windowing in frequency
-                    acc_fil = np.zeros(len(acc), dtype=float)
-                    for i in range(0,len(f)):
-                        hann_coeff = PSD.hann(f[i], f1, f12, f21, f2)
-                        acc_fil[i] = hann_coeff*acc[i]
+                #     # Hann windowing in frequency
+                #     acc_fil = np.zeros(len(acc), dtype=float)
+                #     for i in range(0,len(f)):
+                #         hann_coeff = PSD.hann(f[i], f1, f12, f21, f2)
+                #         acc_fil[i] = hann_coeff*acc[i]
 
-                    # Convert units to nm/s/s
-                    acc_amp = np.sqrt(acc_fil) * 1.e9
-                    f *= 1000.
-                    acc_win = acc_amp[(f >= param['low_corner']) & (f <= param['high_corner'])]
-                    f = f[(f >= param['low_corner']) & (f <= param['high_corner'])]
+                #     # Convert units to nm/s/s
+                #     acc_amp = np.sqrt(acc_fil) * 1.e9
+                #     f *= 1000.
+                #     acc_win = acc_amp[(f >= param['low_corner']) & (f <= param['high_corner'])]
+                #     f = f[(f >= param['low_corner']) & (f <= param['high_corner'])]
                     
-                    # Check if we can do barometric corrections
-                    try:
-                        pr = stations.pressure[0]
-                        print("This station has also pressure data, can do corrections")
+                #     # Check if we can do barometric corrections
+                #     try:
+                #         pr = stations.pressure[0]
+                #         print("This station has also pressure data, can do corrections")
                         
-                        # demean/detrend
-                        pr = pr.detrend('demean')
-                        pr = pr.detrend('linear')
+                #         # demean/detrend
+                #         pr = pr.detrend('demean')
+                #         pr = pr.detrend('linear')
                         
-                        # Windowing
-                        pr = PSD.trim_tr(pr, param['start_time_series'], param['end_time_series'], plot=False) # Trim
-                        pr = pr.taper(type=param['taper_type'], max_percentage=fact)
-                        pr = pr.detrend('linear')
+                #         # Windowing
+                #         pr = PSD.trim_tr(pr, param['start_time_series'], param['end_time_series'], plot=False) # Trim
+                #         pr = pr.taper(type=param['taper_type'], max_percentage=fact)
+                #         pr = pr.detrend('linear')
                         
-                        # Compute FFT
-                        f, pres = periodogram(pr.data, fs=pr.stats.sampling_rate, nfft=NFFT,
-                                           scaling='spectrum')
-                        f, pres = f[1:], pres[1:]
+                #         # Compute FFT
+                #         f, pres = periodogram(pr.data, fs=pr.stats.sampling_rate, nfft=NFFT,
+                #                            scaling='spectrum')
+                #         f, pres = f[1:], pres[1:]
 
-                        # Hann windowing in the frequency domain
-                        pres_fil = np.zeros(len(acc), dtype=float)
-                        for i in range(0,len(f)):
-                            hann_coeff = PSD.hann(f[i], f1, f12, f21, f2)
-                            pres_fil[i] = hann_coeff*pres[i]
+                #         # Hann windowing in the frequency domain
+                #         pres_fil = np.zeros(len(acc), dtype=float)
+                #         for i in range(0,len(f)):
+                #             hann_coeff = PSD.hann(f[i], f1, f12, f21, f2)
+                #             pres_fil[i] = hann_coeff*pres[i]
 
-                        pres_amp = np.sqrt(pres_fil)
-                        f *= 1000. # We back in mHz
-                        pres_win = pres_amp[(f >= param['low_corner']) & (f <= param['high_corner'])]
-                        f = f[(f >= param['low_corner']) & (f <= param['high_corner'])]
+                #         pres_amp = np.sqrt(pres_fil)
+                #         f *= 1000. # We back in mHz
+                #         pres_win = pres_amp[(f >= param['low_corner']) & (f <= param['high_corner'])]
+                #         f = f[(f >= param['low_corner']) & (f <= param['high_corner'])]
 